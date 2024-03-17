c196fce967f62b9a2b535a9f5f6cf594ab95823c
mrcandie~ <akinwumiadekanmi2@gmail.com>
2024-03-17 10:50:40 +0100

    customized icon to weather condition and made the screen responsive on mobile devices

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -18,36 +18,75 @@ function ReusableElement({ title, text, src, onClick }) {
 export default function Weather() {
   const api_key = "ddb927e66d09d3cc100d73bf4ddad767";
   const open_weather_api_key = "0656a9ce39cdaae77b2656c4cc70e069";
+
+  // state to get entered search value
   const [search, setSearch] = useState("");
+
+  // state to store user weather information
   const [data, setData] = useState("");
+
+  // state to store search location's weather information
   const [detail, setDetail] = useState("");
+
+  // state to check if the user searched for a location or it is the users location
   const [initial, setInitial] = useState(true);
+
+  // state for wind speed
   const [speed, setSpeed] = useState("");
+
+  // state for wind speed unit
   const [unit, setUnit] = useState("km/hr");
+
+  //state for temperature unit
   const [tempUnit, setTempUnit] = useState("°C");
+
+  // state for temperature
   const [temp, setTemp] = useState("");
+
+  // state for temperature icon
   const [src, setSrc] = useState("");
 
   useEffect(() => {
-    const initialTemp = initial
-      ? data?.main?.temp
-        ? Math.round(+data?.main?.temp - 273.15)
-        : 0
-      : detail?.current?.temperature;
-
-    if (initialTemp <= 0) {
-      setSrc("/snow.png");
-    } else if (initialTemp > 25 && initialTemp <= 30) {
-      setSrc("/cloud.png");
-    } else if (initialTemp > 1 && initialTemp <= 25) {
-      setSrc("/rain.png");
-    } else if (initialTemp > 30) {
-      setSrc("/clear.png");
+    if (initial) {
+      if (data?.weather[0].icon === "01d" || data?.weather[0].icon === "01n") {
+        setSrc("/clear.png");
+      } else if (
+        data?.weather[0].icon === "02d" ||
+        data?.weather[0].icon === "02n"
+      ) {
+        setSrc("/cloud.png");
+      } else if (
+        data?.weather[0].icon === "03d" ||
+        data?.weather[0].icon === "03n"
+      ) {
+        setSrc("/drizzle.png");
+      } else if (
+        data?.weather[0].icon === "04d" ||
+        data?.weather[0].icon === "04n"
+      ) {
+        setSrc("/drizzle.png");
+      } else if (
+        data?.weather[0].icon === "09d" ||
+        data?.weather[0].icon === "09n"
+      ) {
+        setSrc("/rain.png");
+      } else if (
+        data?.weather[0].icon === "10d" ||
+        data?.weather[0].icon === "10n"
+      ) {
+        setSrc("/rain.png");
+      } else if (
+        data?.weather[0].icon === "13d" ||
+        data?.weather[0].icon === "13n"
+      ) {
+        setSrc("/snow.png");
+      } else {
+        setSrc("/clear.png");
+      }
+    } else {
+      setSrc(detail?.current?.weather_icons[0]);
     }
-    console.log(initialTemp);
   }, [data, initial, detail]);
-
-  console.log(src);
 
   useEffect(() => {
     setSpeed(initial ? data?.wind?.speed : detail?.current?.wind_speed);
@@ -63,6 +102,7 @@ export default function Weather() {
     );
   }, [data, detail, initial]);
 
+  // user's location weather information
   useEffect(() => {
     async function fetchData() {
       try {
@@ -84,11 +124,11 @@ export default function Weather() {
               setInitial(true);
             },
             (error) => {
-              console.error("Error getting user location:", error);
+              toast.error("Error getting user location:", error);
             }
           );
         } else {
-          console.error("Geolocation is not supported by this browser.");
+          toast.error("Geolocation is not supported by this browser.");
         }
       } catch (error) {
         toast.error(error?.response?.data.message || "something went wrong");
@@ -108,7 +148,6 @@ export default function Weather() {
         }
       );
       const data = response.data;
-      console.log(data);
     } catch (error) {
       toast?.error(error?.response?.data.message || "something went wrong");
     }
@@ -116,6 +155,7 @@ export default function Weather() {
 
   async function searchHandler() {
     if (!search) {
+      toast.warning("Enter a valid address");
       return;
     }
     try {
@@ -146,7 +186,7 @@ export default function Weather() {
           onChange={(e) => setSearch(e.target.value)}
           value={search}
           type="text"
-          placeholder="Search"
+          placeholder="Search Address"
         />
         <button onClick={searchHandler}>
           <img src="/search.png" alt="search" />
@@ -201,12 +241,10 @@ export default function Weather() {
         />
         <ReusableElement
           onClick={() => {
-            console.log(123);
             const speed = initial
               ? data?.wind?.speed
               : detail?.current?.wind_speed;
             if (unit === "m/hr") {
-              console.log(speed);
               setSpeed(speed);
               setUnit("km/hr");
             } else {
